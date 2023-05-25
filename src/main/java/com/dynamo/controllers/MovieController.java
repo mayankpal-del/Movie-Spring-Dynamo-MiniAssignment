4771ec67880068fb22ba375a48ca2a1d0da391bf
mayankpal-del <maypal@deloitte.com>
2023-05-25 22:55:52 +0530

    Initial commit

---
+++ src/main/java/com/dynamo/controllers/MovieController.java
@@ -0,0 +1,77 @@
+package com.dynamo.controllers;
+
+import com.dynamo.models.Movie;
+import com.dynamo.services.MovieService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import software.amazon.awssdk.services.dynamodb.model.QueryResponse;
+
+
+import java.util.List;
+@RestController
+@RequestMapping("api/movies")
+public class MovieController {
+    private static Logger logger = LoggerFactory.getLogger(MovieService.class);
+
+    @Autowired
+    private MovieService movieService;
+
+//    @Autowired
+//    public MovieController(MovieService movieService) {
+//        this.movieService = movieService;
+//    }
+
+    @PostMapping("/importCsv")
+    public List<Movie> importCsv(){
+        return movieService.readCsv();
+    }
+
+
+
+    @PostMapping
+    public Movie save(@RequestBody Movie movie){
+        logger.info("save movie " + this.getClass().getName());
+        return movieService.save(movie);
+    }
+
+    @GetMapping("/{id}")
+    public Movie findById(@PathVariable(value = "id") String id){
+        logger.info("find movie by id" + this.getClass().getName());
+        return movieService.findById(id);
+    }
+
+    @GetMapping
+    public List<Movie> findAll(){
+        logger.info("findAll movies " + this.getClass().getName());
+        return movieService.findAll();
+    }
+
+//    @PutMapping("/{id}")
+//    public String update(@PathVariable(value = "id") String id,
+//                         @RequestBody Movie movie){
+//        logger.info("update movie " + this.getClass().getName());
+//        return movieService.update(id, movie);
+//    }
+
+    @DeleteMapping("/{id}")
+    public String delete(@PathVariable(value = "id") String id){
+        logger.info("delete movie " + this.getClass().getName());
+        return movieService.delete(id);
+    }
+    @GetMapping("/director/{director}/year-range/{startYear}/{endYear}")
+    public List<String> getMoviesByDirectorAndYearRange(@PathVariable String director,
+                                                        @PathVariable int startYear,
+                                                        @PathVariable int endYear) {
+        return movieService.getdirector(director, startYear, endYear);
+    }
+
+    @GetMapping("/english-titles")
+    public List<String> getEnglishMoviesWithUserReviewsGreaterThan(
+            @RequestParam int userReviewFilter){
+        return movieService.getEnglishTitlesWithUserReviewsGreaterThan(userReviewFilter);
+    }
+}
+
+
---
+++ src/main/java/com/dynamo/services/MovieService.java
@@ -0,0 +1,214 @@
+package com.dynamo.services;
+
+import com.dynamo.models.Movie;
+import com.dynamo.repository.MovieRepository;
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvValidationException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.dynamodb.model.*;
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.net.URI;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Service
+public class MovieService {
+    private static Logger logger = LoggerFactory.getLogger(MovieService.class);
+
+    @Autowired
+    private MovieRepository movieRepository;
+
+    public Movie save(Movie movie){
+        logger.info("save movie " + this.getClass().getName());
+        return movieRepository.save(movie);
+    }
+//    public List<Movie> readCsv() {
+//        List<Movie> movies = new ArrayList<>();
+
+
+        public List<Movie> readCsv(){
+        List<Movie> newMovies = new ArrayList<>();
+        String[] record =new String[20];
+        try {
+            Reader reader = Files.newBufferedReader(Paths.get("C:\\Users\\maypal\\Downloads\\movies.csv"));
+            CSVReader csvReader = new CSVReader(reader);
+            String[] rec;
+            while ((rec = csvReader.readNext()) != null) {
+                Movie movie = new Movie();
+                movie.setImdb_title_id(rec[0]);
+
+                movie.setTitle(rec[1]);
+                movie.setOriginal_title(rec[2]);
+                movie.setYear(rec[3]);
+                movie.setDate_published(rec[4]);
+                movie.setGenre(rec[5]);
+                movie.setDuration(rec[6]);
+                movie.setCountry(rec[7]);
+                movie.setLanguage(rec[8]);
+                movie.setDirector(rec[9]);
+                movie.setWriter(rec[10]);
+                movie.setProduction_company(rec[11]);
+                movie.setActors(rec[12]);
+                movie.setDescription(rec[13]);
+                movie.setAvg_vote(rec[14]);
+                movie.setVotes(rec[15]);
+                movie.setBudget(rec[16]);
+                movie.setUsa_gross_income(rec[17]);
+                movie.setWorldwide_gross_income(rec[18]);
+                movie.setMetascore(rec[19]);
+                movie.setReviews_from_users(rec[20]);
+                movie.setReviews_from_critics(rec[21]);
+                movieRepository.save(movie);
+                newMovies.add(movie);
+            }
+            csvReader.close();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        } catch (CsvValidationException e) {
+            throw new RuntimeException(e);
+        }
+        return newMovies;
+    }
+
+    public List<Movie> saveAll(List<Movie> movieList){
+        logger.info("save movie " + this.getClass().getName());
+        return  movieRepository.saveAll(movieList);
+    }
+
+    public Movie findById(String id){
+        logger.info("find movie by id" + this.getClass().getName());
+        return movieRepository.findById(id);
+    }
+
+    public List<Movie> findAll(){
+        logger.info("findAll movies " + this.getClass().getName());
+        return movieRepository.findAll();
+    }
+
+//    public String update(String id, Movie movie){
+//        logger.info("update movie " + this.getClass().getName());
+//        return movieRepository.update(id, movie);
+//    }
+//..........................................................................................
+    public String delete(String id){
+        logger.info("Edit Configurations… movie " + this.getClass().getName());
+        return movieRepository.delete(id);
+    }
+
+    public List<String> getdirector(String director, int startYear, int endYear) {
+        String accessKey = "fakeMyKeyId";
+        String secretKey = "fakeSecretAccessKey";
+        String region = "us-east-1";
+        List<String> Titles = new ArrayList<>();
+
+        AwsBasicCredentials credentials = AwsBasicCredentials.create(accessKey, secretKey);
+        StaticCredentialsProvider credentialsProvider = StaticCredentialsProvider.create(credentials);
+
+        DynamoDbClient ddb = DynamoDbClient.builder()
+                .credentialsProvider(credentialsProvider)
+                .region(Region.of(region))
+                .endpointOverride(URI.create("http://localhost:8000")) // Local DynamoDB endpoint
+                .build();
+
+        try {
+            ScanRequest scanRequest = ScanRequest.builder()
+                    .tableName("moviess")
+                    .build();
+            ScanResponse response = ddb.scan(scanRequest);
+            for (Map<String, AttributeValue> item : response.items()) {
+                String title = null;
+                String itemDirector = item.get("director").s();
+                if(director.equals(itemDirector) ){
+                    int year = Integer.parseInt(item.get("year").s());
+// System.out.println(year);
+                    if(startYear < year && endYear > year) {
+                        title = item.get("title").s();
+                        Titles.add(title);
+                    }
+                }
+            }
+
+        } catch (
+                DynamoDbException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+        return Titles;
+    }
+
+
+    public List<String> getEnglishTitlesWithUserReviewsGreaterThan(int userReviewFilter) {
+        String accessKey = "fakeMyKeyId";
+        String secretKey = "fakeSecretAccessKey";
+        String region = "mumbai";
+        List<String> titles = new ArrayList<>();
+
+        AwsBasicCredentials credentials = AwsBasicCredentials.create(accessKey, secretKey);
+        StaticCredentialsProvider credentialsProvider = StaticCredentialsProvider.create(credentials);
+
+        DynamoDbClient ddb = DynamoDbClient.builder()
+                .credentialsProvider(credentialsProvider)
+                .region(Region.of(region))
+                .endpointOverride(URI.create("http://localhost:8000")) // Local DynamoDB endpoint
+                .build();
+        try {
+            ScanRequest scanRequest = ScanRequest.builder()
+                    .tableName("moviess")
+                    .build();
+            ScanResponse response = ddb.scan(scanRequest);
+            List<Map<String, AttributeValue>> items = response.items();
+
+// Filter English titles with user reviews greater than the given filter
+            List<Map<String, AttributeValue>> filteredItems = items.stream()
+                    .filter(item -> {
+                        AttributeValue languageValue = item.get("language");
+                        AttributeValue reviewsValue = item.get("reviews_from_users");
+
+// Check if the attribute values are not null
+                        if (languageValue != null && reviewsValue != null) {
+                            String language = languageValue.s();
+                            String reviews = reviewsValue.s();
+                            if ("English".equals(language) && Integer.parseInt(reviews) > userReviewFilter)
+                                System.out.println(item);
+                            return "English".equals(language) && Integer.parseInt(reviews) > userReviewFilter;
+                        }
+// Skip items with missing or null attribute values
+                        return false;
+                    })
+                    .collect(Collectors.toList());
+// Sort the filtered items by user reviews in descending order
+            filteredItems.sort((item1, item2) -> Integer.compare(
+                    Integer.parseInt(item2.get("reviews_from_users").s()),
+                    Integer.parseInt(item1.get("reviews_from_users").s())));
+
+            for (Map<String, AttributeValue> item : filteredItems) {
+                String title = item.get("title").s();
+                System.out.println(item);
+                titles.add(title);
+            }
+
+        } catch (DynamoDbException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+        return titles;
+
+    }
+
+
+
+}
